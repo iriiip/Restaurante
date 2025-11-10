6627d058233ba309e837a43d87cd45d2ab98e9b4
iriiip <uriel.lopez.vidal@ces-vegamedia.es>
2025-11-10 18:55:42 +0100

    10-11-25

--- cliente/index.php
+++ cliente/index.php
@@ -1,8 +1,10 @@
 <?php
 include("seguridad.php");
-if (isset($_SESSION['mesa']) && isset($_SESSION['comensales'])) {
-    header("LOCATION:carta.php");
-}
+//$dni = $_SESSION['dni'];
+//$consulta = "SELECT estado FROM pedido WHERE dni='$dni' && NOT estado=2";
+//$result = mysqli_query($conn,$consulta);
+//if (mysqli_num_rows($result)!=0)
+//    header("LOCATION:carta.php");
 ?>
 
 <!-- Head -->
